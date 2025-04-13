7d1dcfaf9c380779af789efbaae73a77ac103801
Galushko Ilya <63841534+atmosone@users.noreply.github.com>
2025-04-13 14:04:38 +0000

    v0.0.1 init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/atmosone/goard
+
+go 1.24.1
---
+++ goard.go
@@ -0,0 +1,2 @@
+/* Package goard implements simple authorization/authentication framework for your Go App with session HTTP cookies */
+package goard
